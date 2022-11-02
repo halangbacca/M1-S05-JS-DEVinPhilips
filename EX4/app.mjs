cf1cd64fbbe5f0a70a9068ade253b330f2868b30
Halan <halanbacca@outlook.com>
2022-11-02 17:41:43 -0300

    Exercícios realizados

--- EX4/app.mjs
+++ EX4/app.mjs
@@ -4,3 +4,5 @@ const snoopy = new Cachorro("snoopy", 10);
 console.log(snoopy.som); // "latido"
 const frajola = new Gato("frajola", 8);
 console.log(frajola.som); // "miado"
+console.log(frajola.idade); // 8
+
