58db827eb2a6f4807e01b77371a43ce2dbed935d
Halan <halanbacca@outlook.com>
2022-11-04 21:26:34 -0300

    Exercícios realizados

--- EX7/app.mjs
+++ EX7/app.mjs
@@ -1,11 +1,22 @@
 import Time from "./time.mjs";
 import Partida from "./partida.mjs";
 
-const aplicacaoA = new Juros(10000, 0.07, 24);
-const aplicacaoB = new Juros(10000, 0.125, 120);
+const gre = new Time("Grêmio", "GRE");
+const int = new Time("Internacional", "INT");
 
-console.log(aplicacaoA.calcularJurosSimples());
-console.log(aplicacaoA.calcularJurosCompostos());
+const p1 = new Partida("GRE", 2, "INT", 3);
+const p2 = new Partida("SAN", 1, "SAO", 1);
+const p3 = new Partida("INT", 1, "SAO", 2);
+const p4 = new Partida("SAN", 0, "GRE", 2);
 
-console.log(aplicacaoB.calcularJurosSimples());
-console.log(aplicacaoB.calcularJurosCompostos());
+gre.computarPartida(p1);
+int.computarPartida(p1);
+gre.computarPartida(p2);
+int.computarPartida(p2);
+gre.computarPartida(p3);
+int.computarPartida(p3);
+gre.computarPartida(p4);
+int.computarPartida(p4);
+
+gre.exibirSituacao();
+int.exibirSituacao();
