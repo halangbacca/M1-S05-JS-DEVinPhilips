4935b694951d5906fc6dd8152e3293e8ea2175d4
Halan <halanbacca@outlook.com>
2022-11-06 11:05:57 -0300

    Exercícios realizados

--- EX7/app.mjs
+++ EX7/app.mjs
@@ -1,22 +1,26 @@
 import Time from "./time.mjs";
 import Partida from "./partida.mjs";
 
-const gre = new Time("Grêmio", "GRE");
-const int = new Time("Internacional", "INT");
+const times = [
+  new Time("Grêmio", "GRE"),
+  new Time("Internacional", "INT"),
+  new Time("Santos", "SAN"),
+  new Time("São Paulo", "SAO"),
+];
 
-const p1 = new Partida("GRE", 2, "INT", 3);
-const p2 = new Partida("SAN", 1, "SAO", 1);
-const p3 = new Partida("INT", 1, "SAO", 2);
-const p4 = new Partida("SAN", 0, "GRE", 2);
+const partidas = [
+  new Partida("GRE", 2, "INT", 3),
+  new Partida("SAN", 1, "SAO", 1),
+  new Partida("INT", 1, "SAO", 2),
+  new Partida("SAN", 0, "GRE", 1),
+];
 
-gre.computarPartida(p1);
-int.computarPartida(p1);
-gre.computarPartida(p2);
-int.computarPartida(p2);
-gre.computarPartida(p3);
-int.computarPartida(p3);
-gre.computarPartida(p4);
-int.computarPartida(p4);
+partidas.forEach((partida) => {
+  times.forEach((time) => {
+    time.computarPartida(partida);
+  });
+});
 
-gre.exibirSituacao();
-int.exibirSituacao();
+times.forEach((time) => {
+  time.exibirSituacao();
+});
